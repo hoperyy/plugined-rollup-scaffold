b77e04e6f0731c9a1c491551ed808228972dd9e8
hoperyy <antfootmail@gmail.com>
2020-07-31 18:13:10 +0800

    init

--- index.ts
+++ index.ts
@@ -2,8 +2,11 @@ import Base from './base/index';
 
 import PluginEntry from './plugins/entry';
 
+// get plugins
+const plugins = [
+    PluginEntry
+];
+
 new Base({
-    plugins: [
-        PluginEntry
-    ]
+    plugins
 });
